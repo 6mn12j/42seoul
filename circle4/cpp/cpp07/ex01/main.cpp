2c5cacd01be4d2dc8722665d7468a0f54f27e0a3
6mn12j <dbwk0404@hanmail.net>
2022-09-26 17:36:15 +0900

    cpp 07

--- circle4/cpp/cpp07/ex01/main.cpp
+++ circle4/cpp/cpp07/ex01/main.cpp
@@ -2,26 +2,25 @@
 #include <string>
 #include <iostream>
 
-
-
-int main( void ) {
+int main(void)
+{
 	{
-		int array[] = { 1,2,3,4,5 };
-		iter(array, sizeof(array)/sizeof(int), ftPrint);
+		int array[] = {1, 2, 3, 4, 5};
+		iter(array, sizeof(array) / sizeof(int), ftPrint);
 	}
 
-		std::cout << std::endl;
+	std::cout << std::endl;
 
 	{
 		double array[] = {0.1, 0.2, 0.3, 0.4, 0.5};
-		iter(array, sizeof(array)/sizeof(double), ftPrint);
+		iter(array, sizeof(array) / sizeof(double), ftPrint);
 	}
 
-		std::cout << std::endl;
+	std::cout << std::endl;
 
 	{
 		std::string array[] = {"a", "b", "c", "d", "e"};
-		iter(array, sizeof(array)/sizeof(std::string), ftPrint);
+		iter(array, sizeof(array) / sizeof(std::string), ftPrint);
 	}
 
 	return 0;
--- circle4/cpp/cpp07/ex02/main.cpp
+++ circle4/cpp/cpp07/ex02/main.cpp
@@ -1,45 +1,54 @@
 #include <iostream>
 #include "Array.hpp"
 
-#define LEN   5
-int main() {
+#define LEN 5
+int main()
+{
   std::cout << "---------- int ----------" << std::endl;
-  try {
+  try
+  {
     Array<int> arrayInt(LEN);
     for (int i = 0; i < LEN; i++)
       arrayInt[i] = i + 1;
     for (int i = 0; i < LEN; i++)
       std::cout << arrayInt[i] << " ";
     std::cout << std::endl;
-  } catch(std::exception& e) {
+  }
+  catch (std::exception &e)
+  {
     std::cerr << e.what() << std::endl;
   }
   std::cout << "---------- const int ----------" << std::endl;
-  try {
- 	Array<int> arrayInt(LEN);
-	for (int i = 0; i < LEN; i++)
-    	arrayInt[i] = i + 1;
+  try
+  {
+    Array<int> arrayInt(LEN);
+    const Array<int> arrayIntCopy(arrayInt);
 
-	const Array<int> arrayIntCopy(arrayInt);
+    for (int i = 0; i < LEN; i++)
+      arrayInt[i] = i + 1;
 
-	for (int i = 0; i < LEN; i++)
+    for (int i = 0; i < LEN; i++)
       std::cout << arrayInt[i] << " ";
     std::cout << std::endl;
     for (int i = 0; i < LEN; i++)
       std::cout << arrayIntCopy[i] << " ";
     std::cout << std::endl;
-
-  } catch(std::exception& e) {
+  }
+  catch (std::exception &e)
+  {
     std::cerr << e.what() << std::endl;
   }
   std::cout << "---------- error: [] ----------" << std::endl;
-  try {
+  try
+  {
     Array<int> arrayInt(LEN);
     for (int i = 0; i < LEN; i++)
       arrayInt[i] = i + 1;
     std::cout << arrayInt[LEN]; // ERROR !
     std::cout << std::endl;
-  } catch(std::exception& e) {
+  }
+  catch (std::exception &e)
+  {
     std::cerr << e.what() << std::endl;
   }
   return 0;
